0cb55d949a98ea54df29e0a712d70cfc5e8205a5
furina <3502777299@qq.com>
2024-09-12 12:41:18 +0800

    初始化项目

---
+++ Server/XLS/src/test/java/com/xls/app/XlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xls.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
